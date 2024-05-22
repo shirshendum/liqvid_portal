46cc30fd0bc7459eea8360d17c56837da7ab6a79
Shirshendu <shirshendu.mukherji@liqvid.com>
2024-05-23 01:45:16 +0530

    now showing days remaining for each center

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 conf = configparser.ConfigParser()
 config_file_path = os.path.join(os.getcwd(), 'configs', 'config_production_db.ini')
 
-print(config_file_path)
+#print(config_file_path)
 conf.read(config_file_path)
 
 db_host = conf['mysql']['host']
@@ -46,7 +46,7 @@ def index():
     (SELECT region_id, region_name, center_id, center_name, COUNT(DISTINCT user_id) as regd_users,
     COUNT(DISTINCT CASE WHEN user_role = 'INSTRUCTOR' THEN user_id END) as regd_teachers, trainer_limit,
     COUNT(CASE WHEN user_role = 'LEARNER' THEN 1 END) as regd_students, student_limit, 
-    date(center_created_on) as center_created_date, DATE(DATE_ADD(center_created_on, INTERVAL expiry_days DAY)) AS expiry_date, product, license_key
+    date(center_created_on) as center_created_date, DATEDIFF(DATE(DATE_ADD(center_created_on, INTERVAL expiry_days DAY)), CURRENT_DATE()) AS expiry_date, product, license_key
     FROM rpt_users_test WHERE region_name IN (%s) group by region_id, center_id) rut
     LEFT JOIN (SELECT center_id, day, month, year, SUM(actual_seconds)/3600 AS hours_spent, SUM(CASE WHEN user_role = 'INSTRUCTOR' THEN actual_seconds ELSE 0 END)/3600 AS hours_teachers, SUM(CASE WHEN user_role = 'LEARNER' THEN actual_seconds ELSE 0 END)/3600 AS hours_students FROM rpt_hierarchical_usage GROUP BY center_id) rhu on rhu.center_id = rut.center_id
     LEFT JOIN (SELECT center_id, day, month, year, COUNT(*) AS num_logins, COUNT(CASE WHEN user_role = 'INSTRUCTOR' THEN 1 ELSE NULL END) AS teacher_logins, COUNT(CASE WHEN user_role = 'LEARNER' THEN 1 ELSE NULL END) AS student_logins FROM rpt_hierarchical_logins GROUP BY center_id) rhl on rhl.center_id = rut.center_id
@@ -235,7 +235,7 @@ def update_heatmap2():
         
     cursor.close()
     conn.close()
-    df['num_logins'] = df['num_logins'].astype(float)
+    df['num_logins'] = df['num_logins'].astype(int)
     
     if region:
         df_pivot = df.pivot("center_name", "day", "num_logins")
@@ -309,7 +309,7 @@ def filter_data_table():
     (SELECT region_id, region_name, center_id, center_name, COUNT(DISTINCT user_id) as regd_users,
     COUNT(DISTINCT CASE WHEN user_role = 'INSTRUCTOR' THEN user_id END) as regd_teachers, trainer_limit,
     COUNT(CASE WHEN user_role = 'LEARNER' THEN 1 END) as regd_students, student_limit, 
-    date(center_created_on) as center_created_date, DATE(DATE_ADD(center_created_on, INTERVAL expiry_days DAY)) AS expiry_date, product, license_key
+    date(center_created_on) as center_created_date, DATEDIFF(DATE(DATE_ADD(center_created_on, INTERVAL expiry_days DAY)), CURRENT_DATE()) AS expiry_date, product, license_key
     FROM rpt_users_test WHERE region_name = %s 
     group by region_id, center_id) rut
     LEFT JOIN (SELECT center_id, day, month, year, SUM(actual_seconds)/3600 AS hours_spent, SUM(CASE WHEN user_role = 'INSTRUCTOR' THEN actual_seconds ELSE 0 END)/3600 AS hours_teachers, SUM(CASE WHEN user_role = 'LEARNER' THEN actual_seconds ELSE 0 END)/3600 AS hours_students FROM rpt_hierarchical_usage 
